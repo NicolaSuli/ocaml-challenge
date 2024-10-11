50f297a4104f93b14f7f819841f3268ee4468110
NicolaSuli <sulittu24@gmail.com>
2024-10-11 12:16:48 +0200

    soluzioneNand.ml

--- 1/nand/soluzioneNand.ml
+++ 1/nand/soluzioneNand.ml
@@ -1,1 +1,8 @@
 let nand a b = not (a && b);;
+
+let nand2 (a : bool) (b : bool) : bool = if a then (if b then false else true) else true
+
+let nand3 (a : bool) (b : bool) : bool =
+  match (a,b) with
+  | (true,true) -> false 
+  |_ -> true
