50c0b7646c934d90cc8b51cff9e38cce08373d21
NicolaSuli <sulittu24@gmail.com>
2024-12-01 18:54:21 +0100

    soluzioneAlternative.ml

---
+++ 2/alternative/soluzioneAlternative.ml
@@ -0,0 +1,5 @@
+assert (None <|> Some false = Some false);;
+assert (Some true <|> None <|> Some false = Some true);;
+assert (Some 3 <|> None = Some 3);;
+assert (Some "cat" <|> Some "dog" = Some "cat");;
+assert (None <|> None <|> Some "dog" <|> None = Some "dog");;
